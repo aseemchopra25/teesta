1154f99583b074b349c24e0825246c969031da8f
Aseem Chopra <aseemchopra@protonmail.com>
2021-09-04 00:12:25 +0530

    ✨ removed irrelevant files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +11,4 @@ ReactDOM.render(
 );
 
 
-// serviceWorker.register() for app to work offline and load faster. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// change register to unregister to disable this
-serviceWorker.register();
+
