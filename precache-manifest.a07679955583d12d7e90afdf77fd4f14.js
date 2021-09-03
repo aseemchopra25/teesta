6b48ce9de9638886aac24549cd915781b8170489
Aseem Chopra <aseemchopra@protonmail.com>
2021-09-03 23:51:29 +0530

    Updates

---
+++ precache-manifest.a07679955583d12d7e90afdf77fd4f14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138f9013d576c47f4ea4e17510747786",
+    "url": "/teesta/index.html"
+  },
+  {
+    "revision": "fe7ca7dce2f802c87696",
+    "url": "/teesta/static/css/main.31b6137c.chunk.css"
+  },
+  {
+    "revision": "9c32f66c4f4996a89c65",
+    "url": "/teesta/static/js/2.3d16aa20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teesta/static/js/2.3d16aa20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7ca7dce2f802c87696",
+    "url": "/teesta/static/js/main.3cb0346f.chunk.js"
+  },
+  {
+    "revision": "ee087478b57f8f11324e",
+    "url": "/teesta/static/js/runtime-main.78122e04.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/teesta/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
